aa5322b44db17dfdfc5670855d71924e22dac4f6
siwaweswongcharoen <siwaoh@gmail.com>
2014-05-20 23:25:41 +0700

    v3

--- testGit/src/testGit/Student.java
+++ testGit/src/testGit/Student.java
@@ -2,6 +2,15 @@ package testGit;
 
 public class Student {
 	private String name;
+	private String id;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
 
 	public String getName() {
 		return name;
